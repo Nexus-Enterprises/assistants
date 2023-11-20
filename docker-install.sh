a8df7107c68f4e8c0f8adb9aae84ec62096e1b50
J3remias-Barcelos <jeremiasbarceloa96@gmail.com>
2023-11-20 10:34:15 -0300

    Acrescentando funções para melhorar o Script

--- docker-install.sh
+++ docker-install.sh
@@ -7,33 +7,53 @@ VERSAO=17
 # Nome da empresa
 EMPRESA="Nexus"
 
-# Função para registrar mensagens de erro
-log_error() {
-    echo "Erro: $1" >> log.txt
+# Função para verificar se o Docker está pronto
+docker_ready() {
+    docker run hello-world &> /dev/null
 }
+
+# Função para verificar se o MySQL está pronto
+mysql_ready() {
+    while ! docker exec NexusBank mysqladmin --user=root --password=nexus123 --host=localhost ping --silent &> /dev/null; do
+        echo -e "${PURPLE}[${EMPRESA}]:${NC} Aguardando que o MySQL inicie completamente..."
+        sleep 5
+    done
+}
+
+# Atualizar os pacotes do sistema
+echo -e "${PURPLE}[${EMPRESA}]:${NC} Atualizando os pacotes..."
+sudo apt update && sudo apt upgrade -y
+sleep 30
 
 # Mensagem informativa sobre a instalação do Docker
 echo -e "${PURPLE}[${EMPRESA}]:${NC} Instalando o Docker..."
 sudo apt install docker.io
-sleep 10
+while ! docker_ready; do
+    sleep 10
+done
 echo -e "${PURPLE}[${EMPRESA}]:${NC} Docker instalado com sucesso!"
 
 # Iniciar o serviço do Docker
 echo -e "${PURPLE}[${EMPRESA}]:${NC} Iniciando o serviço do Docker..."
 sudo systemctl start docker
 sudo systemctl enable docker
-sleep 10
+while ! docker_ready; do
+    sleep 10
+done
+echo -e "${PURPLE}[${EMPRESA}]:${NC} Docker está pronto!"
 
 # Baixar a imagem do MySQL 5.7
-echo -e "${PURPLE}[${EMPRESA}]:${NC} Baixando a imagem do MySQL 5.7..."
-sudo docker pull mysql:5.7
-sleep 15
-echo -e "${PURPLE}[${EMPRESA}]:${NC} Imagem do MySQL 5.7 baixada com sucesso!"
+echo -e "${PURPLE}[${EMPRESA}]:${NC} Baixando a imagem do MySQL:latest..."
+sudo docker pull mysql:latest
+while ! mysql_ready; do
+    sleep 10
+done
+echo -e "${PURPLE}[${EMPRESA}]:${NC} Imagem do MySQL baixada com sucesso!"
 
 # Criar e executar o container MySQL com o script SQL
 echo -e "${PURPLE}[${EMPRESA}]:${NC} Criando e executando o container MySQL..."
-sudo docker run -d -p 3306:3306 --name NexusBank -e "MYSQL_DATABASE=NEXUS" -e "MYSQL_ROOT_PASSWORD=nexus123" -v /home/ubuntu/assistants:/docker-entrypoint-initdb.d mysql:5.7
-sleep 15
+sudo docker run -d -p 3306:3306 --name NexusBank -e "MYSQL_DATABASE=NEXUS" -e "MYSQL_ROOT_PASSWORD=nexus123" -v /home/ubuntu/assistants:/docker-entrypoint-initdb.d mysql:latest
+sleep 60  
 echo -e "${PURPLE}[${EMPRESA}]:${NC} Container MySQL criado e em execução!"
 
 # Executar o script SQL dentro do container MySQL
--- java-install.sh
+++ java-install.sh
@@ -17,57 +17,46 @@ installJavaNexus() {
     java -version
     if [ $? -eq 0 ]; then
         echo -e "${PURPLE}[${EMPRESA}]:${NC} Você já possui o Java instalado na sua máquina!"
-        echo -e "${PURPLE}[${EMPRESA}]:${NC} Vamos atualizar os pacotes..."
-        sudo apt update && sudo apt upgrade -y
-        clear
-        echo -e "${PURPLE}[${EMPRESA}]:${NC} Pacotes atualizados!"
-        
-        # Baixa o arquivo JAR diretamente do GitHub
-        echo -e "${PURPLE}[${EMPRESA}]:${NC} Baixando o arquivo JAR..."
-        wget https://github.com/Nexus-Enterprises/login-Java/raw/main/Nexus/target/Nexus-1.0-jar-with-dependencies.jar
-        if [ $? -eq 0 ]; then
-            echo -e "${PURPLE}[${EMPRESA}]:${NC} Arquivo JAR baixado com sucesso!"
-            echo -e "${PURPLE}[${EMPRESA}]:${NC} Agora vamos executar o arquivo JAR..."
-            java -jar Nexus-1.0-jar-with-dependencies.jar
-        else
-            log_error "Erro ao baixar o arquivo JAR do GitHub"
-        fi
     else
         echo -e "${PURPLE}[${EMPRESA}]:${NC} Não foi encontrada nenhuma versão do Java na sua máquina, mas iremos resolver isso!"
-        echo -e "${PURPLE}[${EMPRESA}]:${NC} Você deseja instalar o Java na sua máquina (S/N)?"
-        read inst
-        if [ "$inst" == "S" ]; then
-            echo -e "${PURPLE}[${EMPRESA}]:${NC} Ok! Você decidiu instalar o Java na máquina, uhul!"
-            echo -e "${PURPLE}[${EMPRESA}]:${NC} Adicionando o repositório!"
-            sleep 2
-            sudo add-apt-repository ppa:linuxuprising/java
-            clear
-            echo -e "${PURPLE}[${EMPRESA}]:${NC} Atualizando os pacotes... Quase acabando."
-            sleep 2
-            sudo apt update -y
-            clear
-            if [ $VERSAO -eq 17 ]; then
-                echo -e "${PURPLE}[${EMPRESA}]:${NC} Preparando para instalar a versão 17 do Java. Lembre-se de confirmar a instalação quando necessário!"
-                sudo apt-get install openjdk-17-jdk
-                clear
-                echo -e "${PURPLE}[${EMPRESA}]:${NC} Java instalado com sucesso!"
-                echo -e "${PURPLE}[${EMPRESA}]:${NC} Vamos atualizar os pacotes..."
-                sudo apt update && sudo apt upgrade -y
-                clear
-
-                # Baixa o arquivo JAR diretamente do GitHub
-                echo -e "${PURPLE}[${EMPRESA}]:${NC} Baixando o arquivo JAR..."
-                wget https://github.com/Nexus-Enterprises/login-Java/raw/main/Nexus/target/Nexus-1.0-jar-with-dependencies.jar
-                if [ $? -eq 0 ]; then
-                    echo -e "${PURPLE}[${EMPRESA}]:${NC} Arquivo JAR baixado com sucesso!"
-                    echo -e "${PURPLE}[${EMPRESA}]:${NC} Agora vamos executar o arquivo JAR..."
-                    java -jar Nexus-1.0-jar-with-dependencies.jar
-                else
-                    log_error "Erro ao baixar o arquivo JAR do GitHub"
-                fi
+        while true; do
+            echo -e "${PURPLE}[${EMPRESA}]:${NC} Você deseja instalar o Java na sua máquina (S/N)?"
+            read inst
+            if [ "$inst" == "S" ]; then
+                break
+            elif [ "$inst" == "N" ]; then
+                echo -e "${PURPLE}[${EMPRESA}]:${NC} Você optou por não instalar o Java por enquanto."
+                return
+            else
+                echo -e "${PURPLE}[${EMPRESA}]:${NC} Opção inválida. Por favor, insira 'S' para sim ou 'N' para não."
             fi
-        else
-            log_error "Você optou por não instalar o Java por enquanto."
+        done
+
+        echo -e "${PURPLE}[${EMPRESA}]:${NC} Ok! Você decidiu instalar o Java na máquina, uhul!"
+        echo -e "${PURPLE}[${EMPRESA}]:${NC} Adicionando o repositório!"
+        sudo add-apt-repository ppa:linuxuprising/java
+        sudo apt update -y
+        clear
+
+        if [ $VERSAO -eq 17 ]; then
+            echo -e "${PURPLE}[${EMPRESA}]:${NC} Preparando para instalar a versão 17 do Java. Lembre-se de confirmar a instalação quando necessário!"
+            sudo apt-get install openjdk-17-jdk -y
+            clear
+
+            echo -e "${PURPLE}[${EMPRESA}]:${NC} Java instalado com sucesso!"
+            echo -e "${PURPLE}[${EMPRESA}]:${NC} Vamos atualizar os pacotes..."
+            sudo apt update && sudo apt upgrade -y
+            clear
+
+            echo -e "${PURPLE}[${EMPRESA}]:${NC} Baixando o arquivo JAR..."
+            wget https://github.com/Nexus-Enterprises/login-Java/raw/main/Nexus/target/Nexus-1.0-jar-with-dependencies.jar
+            if [ $? -eq 0 ]; then
+                echo -e "${PURPLE}[${EMPRESA}]:${NC} Arquivo JAR baixado com sucesso!"
+                echo -e "${PURPLE}[${EMPRESA}]:${NC} Agora vamos executar o arquivo JAR..."
+                java -jar Nexus-1.0-jar-with-dependencies.jar
+            else
+                echo -e "${PURPLE}[${EMPRESA}]:${NC} Erro ao baixar o arquivo JAR do GitHub"
+            fi
         fi
     fi
 }
